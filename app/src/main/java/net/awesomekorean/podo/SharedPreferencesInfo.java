28143710891076afcf56d56063f830a309f94a48
kammaii <kammaii@naver.com>
2021-04-02 23:44:18 +0700

    fixed SharedPreferencesInfo

--- app/src/main/java/net/awesomekorean/podo/SharedPreferencesInfo.java
+++ app/src/main/java/net/awesomekorean/podo/SharedPreferencesInfo.java
@@ -15,7 +15,7 @@ public class SharedPreferencesInfo {
     static Gson gson = new GsonBuilder().create();
 
 
-    // 앱에 유저 데이터 저장하기
+    // 유저 데이터
     public static void setUserInfo(Context context, UserInformation userInformation) {
         String stringToSP = gson.toJson(userInformation, UserInformation.class);
         SharedPreferences sp = context.getSharedPreferences("user", MODE_PRIVATE);
@@ -23,8 +23,6 @@ public class SharedPreferencesInfo {
         editor.putString("info", stringToSP);
         editor.commit();
     }
-
-    // 엡에서 유저 데이터 불러오기
     public static UserInformation getUserInfo(Context context) {
         SharedPreferences sp = context.getSharedPreferences("user", MODE_PRIVATE);
         String stringFromSP = sp.getString("info", null);
@@ -32,49 +30,34 @@ public class SharedPreferencesInfo {
         return userInformation;
     }
 
-    // 엡에서 유저 이메일 불러오기
+
+    // 유저 이메일
     public static String getUserEmail(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("userEmail", MODE_PRIVATE);
-        String stringFromSP = sp.getString("userEmail", null);
-        return stringFromSP;
+        String result = getString(context, "userEmail");
+        return result;
     }
-
-    // 엡에 유저 이메일 저장하기
     public static void setUserEmail(Context context, String userEmail) {
-        SharedPreferences sp = context.getSharedPreferences("userEmail", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString("userEmail", userEmail);
-        editor.commit();
+        setString(context, "userEmail", userEmail);
     }
 
-    // 엡에서 유저 이름 불러오기
+
+    // 유저 이름
     public static String getUserName(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("userName", MODE_PRIVATE);
-        String stringFromSP = sp.getString("userName", null);
-        return stringFromSP;
+        String result = getString(context, "userName");
+        return result;
     }
-
-    // 엡에 유저 이름 저장하기
     public static void setUserName(Context context, String userName) {
-        SharedPreferences sp = context.getSharedPreferences("userName", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString("userName", userName);
-        editor.commit();
+        setString(context, "userName", userName);
     }
 
-    // 엡에서 유저 토큰 불러오기
+
+    // 유저 토큰
     public static String getUserToken(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("userToken", MODE_PRIVATE);
-        String stringFromSP = sp.getString("userToken", null);
-        return stringFromSP;
+        String result = getString(context, "userToken");
+        return result;
     }
-
-    // 엡에 유저 토큰 저장하기
     public static void setUserToken(Context context, String userToken) {
-        SharedPreferences sp = context.getSharedPreferences("userToken", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString("userToken", userToken);
-        editor.commit();
+        setString(context, "userToken", userToken);
     }
 
 
@@ -90,7 +73,6 @@ public class SharedPreferencesInfo {
         editor.commit();
         System.out.println("클릭 위치를 저장했습니다: "+pos);
     }
-
     public static int getLastClickItem(Context context, boolean isLesson) {
         SharedPreferences sp = context.getSharedPreferences("lastClickedItem", MODE_PRIVATE);
         int lastPos;
@@ -104,29 +86,21 @@ public class SharedPreferencesInfo {
     }
 
 
-    // 마지막으로 클릭한 레슨 레벨 저장하기
+    // 마지막으로 클릭한 레슨 레벨
     public static void setLastClickLevel(Context context, int level) { // 0:초급, 1:중급, 2:고급
-        SharedPreferences sp = context.getSharedPreferences("lastClickedLevel", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt("level", level);
-        editor.commit();
-        System.out.println("클릭 레벨을 저장했습니다: "+level);
+        setInt(context, "lastClickedLevel", level);
     }
-
     public static int getLastClickLevel(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("lastClickedLevel", MODE_PRIVATE);
-        int level = sp.getInt("level", 0);
-        System.out.println("클릭 레벨을 불러옵니다: "+level);
-        return level;
+        int result = getInt(context, "lastClickedLevel");
+        return result;
     }
 
-    // 타이머 불러오기
+
+    // 이벤트 타이머
     public static SharedPreferences getEventTimer(Context context) {
         SharedPreferences sp = context.getSharedPreferences("eventTimer", MODE_PRIVATE);
         return sp;
     }
-
-    // 타이머 저장하기
     public static void setEventTimer(Context context, long eventTime, int percent) {
         SharedPreferences sp = context.getSharedPreferences("eventTimer", MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
@@ -146,49 +120,72 @@ public class SharedPreferencesInfo {
             Bundle bundle = new Bundle();
             firebaseAnalytics.logEvent("eventTimer_finished", bundle);
         }
-
         editor.commit();
     }
 
 
-    // LessonQuiz1 EngHint 저장하기
+    // LessonQuiz1 EngHint
     public static void setEngHint(Context context, boolean b) {
-        SharedPreferences sp = context.getSharedPreferences("engHint", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("engHint", b);
-        editor.commit();
-        System.out.println("EngHint 를 저장했습니다: "+b);
+        setBoolean(context, "engHint", b);
+    }
+    public static boolean getEngHint(Context context) {
+        boolean result = getBoolean(context, "engHint");
+        return result;
     }
 
-    public static boolean getEngHint(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("engHint", MODE_PRIVATE);
-        boolean engHint = sp.getBoolean("engHint", false);
-        System.out.println("EngHint 를 불러옵니다: "+engHint);
-        return engHint;
-    }
 
     // 챌린저 할인 이벤트 대상여부
     public static void setChallengerDiscountAvailable(Context context, boolean b) {
         setBoolean(context, "challengerDiscountAvailable", b);
-        SharedPreferences sp = context.getSharedPreferences("challengerDiscountAvailable", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean("challengerDiscountAvailable", b);
-        editor.commit();
-        System.out.println("EngHint 를 저장했습니다: "+b);
     }
-
     public static boolean getChallengerDiscountAvailable(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("challengerDiscountAvailable", MODE_PRIVATE);
-        boolean challengerDiscountAvailable = sp.getBoolean("challengerDiscountAvailable", false);
-        System.out.println("challengerDiscountAvailable 를 불러옵니다: "+challengerDiscountAvailable);
-        return challengerDiscountAvailable;
+        boolean result = getBoolean(context, "challengerDiscountAvailable");
+        return result;
     }
 
-    public static void setBoolean(Context context, String string, boolean b) {
-        SharedPreferences sp = context.getSharedPreferences(string, MODE_PRIVATE);
+
+    public static void setString(Context context, String name, String string) {
+        SharedPreferences sp = context.getSharedPreferences(name, MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean(string, b);
+        editor.putString(name, string);
         editor.commit();
-        System.out.println(string +"을/를 저장했습니다: "+b);
+    }
+
+    public static String getString(Context context, String name) {
+        SharedPreferences sp = context.getSharedPreferences(name, MODE_PRIVATE);
+        String result = sp.getString(name, null);
+        return result;
+    }
+
+    public static void setInt(Context context, String name, int number) {
+        SharedPreferences sp = context.getSharedPreferences(name, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(name, number);
+        editor.commit();
+        System.out.println(name + "을/를 저장했습니다: " + number);
+    }
+
+
+    public static int getInt(Context context, String name) {
+        SharedPreferences sp = context.getSharedPreferences(name, MODE_PRIVATE);
+        int result = sp.getInt(name, 0);
+        System.out.println(name + "을/를 불러옵니다: " + result);
+        return result;
+    }
+
+
+    public static void setBoolean(Context context, String name, boolean b) {
+        SharedPreferences sp = context.getSharedPreferences(name, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(name, b);
+        editor.commit();
+        System.out.println(name +"을/를 저장했습니다: " + b);
+    }
+
+    public static boolean getBoolean(Context context, String name) {
+        SharedPreferences sp = context.getSharedPreferences(name, MODE_PRIVATE);
+        boolean result = sp.getBoolean(name, false);
+        System.out.println(name + "을/를 불러옵니다: " + result);
+        return result;
     }
 }
